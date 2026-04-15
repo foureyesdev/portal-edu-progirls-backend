bfe48c70ff934520408fc4042b14f8b6ea545852
Lorena <111245669+foureyesdev@users.noreply.github.com>
2026-04-15 16:36:58 -0300

    feat: criar entidade Categoria

---
+++ portal/src/main/java/br/com/progirls/api/portal/model/entity/Categoria.java
@@ -0,0 +1,18 @@
+package br.com.progirls.api.portal.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categoria")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Categoria extends EntidadeBase {
+    @Column(length = 50, nullable = false, unique = true)
+    private String nome;
+}

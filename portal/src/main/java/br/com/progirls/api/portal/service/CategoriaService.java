cccee00a669ec69763d5d83c47309a740b65675a
Lorena <111245669+foureyesdev@users.noreply.github.com>
2026-04-16 10:50:32 -0300

    docs: atualizar endpoints do README

--- portal/src/main/java/br/com/progirls/api/portal/service/CategoriaService.java
+++ portal/src/main/java/br/com/progirls/api/portal/service/CategoriaService.java
@@ -2,12 +2,10 @@ package br.com.progirls.api.portal.service;
 
 import br.com.progirls.api.portal.mapper.CategoriaMapper;
 import br.com.progirls.api.portal.model.dto.categoria.CategoriaResponseDTO;
-import br.com.progirls.api.portal.model.entity.Categoria;
 import br.com.progirls.api.portal.repository.CategoriaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
